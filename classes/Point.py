c4d96c275ace30e9346dfa4730fcf34905420995
Sándor Kardos <sandor@goodworks.hu>
2022-03-02 20:53:33 +0100

    Added comments to Point class

--- classes/Point.py
+++ classes/Point.py
@@ -2,18 +2,33 @@ import math
 import random
 
 class Point(object):
+  """Point object.
+    ### Parameters
+    - x: number
+    - y: number
+    """
   def __init__(self, x, y):
     self.x = x
     self.y = y
 
   @staticmethod
   def distance(p1, p2):
-    return math.sqrt(
+    """Euclidean distance between two Points.
+    ### Parameters
+    - p1: Point
+    - p2: Point
+    """
+    return type(math.sqrt(
         pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2)
-    )
+    ))
 
   @staticmethod
   def randomPoint(radius, center):
+    """Random Point within a circle.
+    ### Parameters
+    - radius: number
+    - center: Point
+    """
     random_angle = 2 * math.pi * random.random()
     random_radius = radius * math.sqrt(random.random())
     return Point(
